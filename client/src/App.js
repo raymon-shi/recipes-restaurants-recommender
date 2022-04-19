c53b24f95bd5e4e64eb0587834d6317d6fd6da7d
Raymon Shi <61591942+raymon-shi@users.noreply.github.com>
2022-04-19 00:49:54 -0400

    added initial homepage and search results

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import React, {useState} from 'react';
-import './App.css';
-import Test from './components/Test'
+import logo from './logo.svg'
+import React, { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/home/Home'
+import NavHeader from './components/home/NavHeader'
+import Search from './components/search/Search'
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   const [state, setState] = useState('')
   return (
     <>
-      <Test />
+      <Container>
+        <NavHeader />
+      </Container>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/search' element={<Search />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
